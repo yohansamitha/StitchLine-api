be079ce7f7b1c79dd6612684337ccc752748d423
YohanSamitha <yohansamitha123@gmail.com>
2021-07-26 01:28:31 +0530

    init

---
+++ src/main/kotlin/com/stitchline/stitchlineapi/StitchlineapiApplication.kt
@@ -0,0 +1,11 @@
+package com.stitchline.stitchlineapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StitchlineapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<StitchlineapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/stitchline/stitchlineapi/StitchlineapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stitchline.stitchlineapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StitchlineapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
